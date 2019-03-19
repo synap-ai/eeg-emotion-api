e2750d7acab5c94848c4156f5d792ba950df3c98
Henry Tran <22henrytran@gmail.com>
2019-03-19 13:59:15 -0400

    Update API for arousal and pleasure classifiers

--- run.py
+++ run.py
@@ -1,8 +1,9 @@
-from keras.models import load_model
 import numpy as np
-import tensorflow as tf
 import flask
 import io
+import pickle
+import math
+from collections import defaultdict
 
 # initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
@@ -10,54 +11,116 @@ app.config.update(
     DEBUG=True,
     MAX_CONTENT_LENGTH=50000000
 )
-model = None
+arousal_model = None
+pleasure_model = None
 
-EEG_WINDOW_SIZE = 3564
-EEG_SENSORS = ['tp9', 'af7', 'af8', 'tp10']
+EEG_SENSORS = ['af7', 'af8']
+
+sf = 256
+window = 1.00
+alpha = [8, 12]
+beta = [12, 30]
 
 def load():
-    global model
-    model = load_model('models/eeg_regression_model_2.h5')
-    global graph
-    graph = tf.get_default_graph()
+    global arousal_model
+    arousal_model = pickle.load(open('models/eeg_arousal_classify_model.pickle', 'rb'))
+    global pleasure_model
+    pleasure_model = pickle.load(open('models/eeg_pleasure_classify_model.pickle', 'rb'))
 
 
 def prepare_eeg(eeg):
-    cleaned_eeg = []
+    # TODO Add blink filter
+    eeg_readings = defaultdict(list)
     for sample in eeg:
-        eeg_reading = []
         for k in EEG_SENSORS:
-            eeg_reading.append(sample[k])
-        cleaned_eeg.append(eeg_reading)
-    cleaned_eeg = np.array([cleaned_eeg])
-    # TODO Add blink filter, time stamps
-    cleaned_eeg = cleaned_eeg.reshape((len(cleaned_eeg), len(cleaned_eeg[0]), len(cleaned_eeg[0][0]), 1))
+            if math.isnan(sample[k]):
+                sample[k] = 0
+            eeg_readings[k].append(sample[k])
+    
+    band_powers = []
+    for i in range(0, len(eeg_readings['af7']) - sf, 16):
+        af7 = eeg_readings['af7'][i:i+sf]
+        af8 = eeg_readings['af8'][i:i+sf]
 
-    return cleaned_eeg
+        af7_alpha = bandpower(af7, sf, alpha, window)
+        af7_beta = bandpower(af7, sf, beta, window)
+        af8_alpha = bandpower(af8, sf, alpha, window)
+        af8_beta = bandpower(af8, sf, beta, window)
+        band_powers.append([af7_alpha, af7_beta, af8_alpha, af8_beta])
+
+    return np.array(band_powers)
 
 
-@app.route("/predict", methods=["POST"])
+@app.route('/predict', methods=['POST'])
 def predict():
-    data = {"success": False}
+    data = {'success': False}
 
-    if flask.request.method == "POST":
-        if flask.request.get_json("eeg"):
-            eeg_data = flask.request.get_json("eeg")["eeg"]
-            if len(eeg_data) < EEG_WINDOW_SIZE:
-                data["message"] = "EEG sample size is too small"
-                return flask.jsonify(data)
-
-            eeg_data = prepare_eeg(eeg_data[:EEG_WINDOW_SIZE])
-            with graph.as_default():
-                preds = model.predict(eeg_data)
-                data["predictions"] = [preds.tolist()]
-                data["success"] = True
+    if flask.request.method == 'POST':
+        if flask.request.get_json('eeg'):
+            eeg_data = flask.request.get_json('eeg')['eeg']
+            eeg_data = prepare_eeg(eeg_data)
+            arousal_preds = arousal_model.predict(eeg_data)
+            pleasure_preds = pleasure_model.predict(eeg_data)
+            data['predictions'] = {
+                'arousal': [arousal_preds.tolist()],
+                'pleasure': [arousal_preds.tolist()],
+            }
+            data['success'] = True
 
     return flask.jsonify(data)
 
+def bandpower(data, sf, band, window_sec=None, relative=False):
+    '''Compute the average power of the signal x in a specific frequency band.
 
-if __name__ == "__main__":
-    print(("* Loading Keras model and Flask starting server..."
-           "please wait until server has fully started"))
+    Parameters
+    ----------
+    data : 1d-array
+        Input signal in the time-domain.
+    sf : float
+        Sampling frequency of the data.
+    band : list
+        Lower and upper frequencies of the band of interest.
+    window_sec : float
+        Length of each window in seconds.
+        If None, window_sec = (1 / min(band)) * 2
+    relative : boolean
+        If True, return the relative power (= divided by the total power of the signal).
+        If False (default), return the absolute power.
+
+    Return
+    ------
+    bp : float
+        Absolute or relative band power.
+    '''
+    from scipy.signal import welch
+    from scipy.integrate import simps
+    band = np.asarray(band)
+    low, high = band
+
+    # Define window length
+    if window_sec is not None:
+        nperseg = window_sec * sf
+    else:
+        nperseg = (2 / low) * sf
+
+    # Compute the modified periodogram (Welch)
+    freqs, psd = welch(data, sf, nperseg=nperseg)
+
+    # Frequency resolution
+    freq_res = freqs[1] - freqs[0]
+
+    # Find closest indices of band in frequency vector
+    idx_band = np.logical_and(freqs >= low, freqs <= high)
+
+    # Integral approximation of the spectrum using Simpson's rule.
+    bp = simps(psd[idx_band], dx=freq_res)
+
+    if relative:
+        bp /= simps(psd, dx=freq_res)
+    return bp
+
+if __name__ == '__main__':
+    print(('* Loading model and Flask starting server...'
+           'please wait until server has fully started'))
     load()
     app.run()
