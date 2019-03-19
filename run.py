65c3d253c371ec5005e4082c0719e0e8c439ab4d
Henry Tran <22henrytran@gmail.com>
2019-03-18 22:05:38 -0400

    Clean eeg data before prediction

--- run.py
+++ run.py
@@ -1,34 +1,40 @@
-# USAGE
-# Start the server:
-# 	python run_keras_server.py
-# Submit a request via cURL:
-# 	curl -X POST -F image=@dog.jpg 'http://localhost:5000/predict'
-# Submita a request via Python:
-#	python simple_request.py
-
 from keras.models import load_model
 import numpy as np
+import tensorflow as tf
 import flask
 import io
 
 # initialize our Flask application and the Keras model
 app = flask.Flask(__name__)
+app.config.update(
+    DEBUG=True,
+    MAX_CONTENT_LENGTH=50000000
+)
 model = None
 
 EEG_WINDOW_SIZE = 3564
 
+EEG_SENSORS = ['tp9', 'af7', 'af8', 'tp10']
 
 def load():
     global model
-    model = load_model('models/eeg_regression_model.h5')
+    model = load_model('models/eeg_regression_model_2.h5')
+    global graph
+    graph = tf.get_default_graph()
 
 
 def prepare_eeg(eeg):
-    # TODO Add blink filter
-    eeg = eeg[:EEG_WINDOW_SIZE]
-    eeg = eeg.reshape((len(eeg), len(eeg[0]), len(eeg[0][0]), 1))
+    cleaned_eeg = []
+    for sample in eeg:
+        eeg_reading = []
+        for k in EEG_SENSORS:
+            eeg_reading.append(sample[k])
+        cleaned_eeg.append(eeg_reading)
+    cleaned_eeg = np.array([cleaned_eeg])
+    # TODO Add blink filter, time stamps
+    cleaned_eeg = cleaned_eeg.reshape((len(cleaned_eeg), len(cleaned_eeg[0]), len(cleaned_eeg[0][0]), 1))
 
-    return eeg
+    return cleaned_eeg
 
 
 @app.route("/predict", methods=["POST"])
@@ -37,12 +43,12 @@ def predict():
 
     if flask.request.method == "POST":
         if flask.request.get_json("eeg"):
-            eeg = np.array(flask.request.get_json["image"])
-            eeg = prepare_eeg(eeg)
+            eeg = prepare_eeg(flask.request.get_json("eeg")["eeg"][:EEG_WINDOW_SIZE])
 
-            pred = model.predict(eeg)
-            data["predictions"] = [pred]
-            data["success"] = True
+            with graph.as_default():
+                preds = model.predict(eeg)
+                data["predictions"] = [preds.tolist()]
+                data["success"] = True
 
     return flask.jsonify(data)
 
